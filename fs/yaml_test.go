87f706da4e257625140a0677027c93c3ae0e1689
Andrea Genovesi <andrea.genovesi@gmail.com>
2020-04-16 22:25:05 +0200

    GU-3 add fs.ExistsPath func

--- fs/yaml_test.go
+++ fs/yaml_test.go
@@ -13,7 +13,7 @@ type Story struct {
 	GoALCmds    []string `yaml:"goalcmds,flow"`
 }
 
-func TestReadCommand(t *testing.T) {
+func TestReadYaml(t *testing.T) {
 	yamlF := "test.yaml"
 	story:= Story{}
 	if err := ReadYaml(yamlF, &story); err != nil {
@@ -23,3 +23,20 @@ func TestReadCommand(t *testing.T) {
 	assert.Equal(t, 4, len(story.GoALCmds), "GoALCmds should be equal 4")
 }
 
+func TestExistsPath(t *testing.T) {
+	yamlF := "test.yaml"
+	var (
+		ok bool
+		err error
+	)
+	if ok,err = ExistsPath(yamlF); err != nil {
+		fmt.Println(err.Error())
+	}
+	// assert equality
+	assert.Equal(t, true, ok, "File test.yaml should exist")
+	yamlF = "test1.yaml"
+	if ok,err = ExistsPath(yamlF); err != nil {
+		fmt.Println(err.Error())
+	}
+	assert.Equal(t, false, ok, "File test.yaml should not exist")
+}
