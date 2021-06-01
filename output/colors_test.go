d003f4d5ff9eb6e1130f49dfad91852956dea13c
mas2020 <andrea.genovesi@gmail.com>
2021-06-01 18:29:58 +0200

    (#10) re-write colors implementation

--- output/colors_test.go
+++ output/colors_test.go
@@ -1,30 +1,22 @@
 package output
 
 import (
-	"github.com/jedib0t/go-pretty/text"
-	log "github.com/sirupsen/logrus"
 	"testing"
 )
 
 func TestColors(t *testing.T) {
-	log.SetLevel(log.TraceLevel)
-	Error("TestColors", "Error message")
-	Debug("TestColors", "Debug message")
-	Trace("TestColors", "Trace message")
-	Green("Green message")
-	GreenBold("Green Bold message")
-	Blue("Blue message")
-	BlueBold("Blue Bold message")
-	Yellow("Yellow message")
-	YellowBold("Yellow Bold message")
-	Cyan("Cyan message")
-	CyanBold("Cyan Bold message")
-	HiWhite("HiWhite message")
-	HiWhiteBold("HiWhiteBold Bold message")
-	HiYellow("HiYellow message")
-	HiYellowBold("HiYellowBold Bold message")
-	HiMagenta("HiMagenta message")
-	HiMagentaBold("HiMagentaBold Bold message")
-	Message("Generic message", text.Colors{text.Bold, text.BgGreen})
-	//ErrorExit("TestColors", "Error message bye bye...")
+	RedText("Red message")
+	RedTextB("Red bold message")
+	GreenText("Green message")
+	GreenTextB("Green bold message")
+	BlueText("Blue message")
+	BlueTextB("Blue bold message")
+	YellowText("Yellow message")
+	YellowTextB("Yellow bold message")
+	CyanText("Cyan message")
+	CyanTextB("Cyan bold message")
+	WhiteText("White message")
+	WhiteTextB("White bold message")
+	MagentaText("Magenta message")
+	MagentaTextB("Magenta bold message")
 }
